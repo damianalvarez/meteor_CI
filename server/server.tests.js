5a2f652a26d7c2ac430677ffb3a91ec58d2bb16d
Damian Alvarez <damian@wasteplace.com>
2018-12-22 17:42:47 -0300

    Add test CI project files

---
+++ server/server.tests.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-env mocha */
+import { expect } from 'chai';
+
+describe('my server test', function () {
+  it('passes because true is true', function () {
+    expect(true).to.be.true;
+  });
+});
